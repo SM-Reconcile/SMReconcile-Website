f1b58970961ffa3dab30963a20ef811d02c20a96
nilay nath sharan <nilay@pop-os.localdomain>
2023-12-10 19:02:24 +0530

    yolo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { PreloadProvider } from "@/context/PreloadContext";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'SMReconcile',
-  description: 'Environment Solutions',
-}
+  title: "SM-Reconcile",
+  description: "SM-Reconcile Environment Solutions",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="white">
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased grainy",
+          inter.className
+        )}
+      >
+        <PreloadProvider>{children}</PreloadProvider>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
+"use client"
+
+import useLoaded from "@/hooks/useLoaded";
+
 
 export default function Home() {
-  return <>hello</>;
+  const isLoaded = useLoaded();
+
+  return <>
+  <div>
+    <h1>Home</h1>
+    <p>Loaded: {isLoaded ? "true" : "false"}</p>
+  </div>
+  </>;
 }
